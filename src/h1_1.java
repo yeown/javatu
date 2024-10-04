2003eff4f4dd440bf303afc11dce2572e8f6b185
yeown <happychris2005@gmail.com>
2024-10-04 22:46:07 +0900

    h1_1

--- src/h1_1.java
+++ src/h1_1.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class h1_1 {
+import java.util.Scanner;
+import java.lang.Math;
+public class h1_1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double V = sc.nextDouble();
+        double R = sc.nextDouble();
+        double W = V*V/R;
+        double dBm = 10*Math.log10(W) + 30;
+        System.out.println("dBm is "+dBm);
+
+    }
 }

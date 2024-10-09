f4b4cd6f2e43ce0259d53d10fe6edc344d1f2cd9
yeown <happychris2005@gmail.com>
2024-10-09 20:22:05 +0900

    h2

--- src/h2/Car.java
+++ src/h2/Car.java
@@ -2,6 +2,43 @@ package h2;
 
 public class Car {
     public Car(String carName, int gas) {
+        setCarName(carName);
+        this.gas = gas;
+    }
+    void setGas(int gas) {
+        this.gas = gas;
+    }
+    int getGas() {
+        return gas;
+    }
+    void setCarName(String carName) {
+        this.carName = carName;
+    }
+    String getCarName() {
+        return carName;
+    }
+    int gas;
+    String carName;
+
+    int move(int gas){
+        if(this.gas>=gas){
+            setGas(this.gas-gas);
+            return getGas();
+        }
+        else{
+            setGas(this.gas);
+            return getGas()-gas;
+        }
+    }
+    int refuel(int amount){
+        setGas(this.gas + amount);
+        return getGas();
+    }
+}
+/*package h2;
+
+public class Car {
+    public Car(String carName, int gas) {
         this.carName = carName;
         this.gas = gas;
     }
@@ -16,17 +53,5 @@ public class Car {
         this.gas = gas + 10;
         return this.gas;
     }
-}
-/*
-void setGas(int gas) {
-    this.gas = gas;
-}
-int getGas() {
-    return gas;
-}
-void setCarName(String carName) {
-    this.carName = carName;
-}
-String getCarName() {
-    return carName;
 }*/
+
--- src/h2/Main.java
+++ src/h2/Main.java
@@ -5,6 +5,35 @@ class Main {
         Car[] cars = new Car[4];
         String[] carNames = {"kia", "ferrari", "bmw", "toyota"};
         int[] carGas = {10, 20, 15, 17};
+
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(carNames[i], carGas[i]);
+        }
+
+        for (int i = 0; i < 7; i++) {
+            for(int j = 0; j < 4; j++) {
+                if(cars[j].move(5)>=0) {
+                    System.out.println(cars[j].getCarName()+" move, resume gas: "+cars[j].getGas());
+                }
+                else{
+                    System.out.println(cars[j].getCarName()+" out of gas, resume gas: "+ cars[j].getGas());
+                    System.out.println("fuel amount: 10, resume fuel: "+cars[j].refuel(10));
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+
+/*
+package h2;
+
+class Main {
+    public static void main(String[] args) {
+        Car[] cars = new Car[4];
+        String[] carNames = {"kia", "ferrari", "bmw", "toyota"};
+        int[] carGas = {10, 20, 15, 17};
         int[] regas = carGas;
 
         for (int i = 0; i < cars.length; i++) {
@@ -30,3 +59,4 @@ class Main {
     }
 }
 
+*/
